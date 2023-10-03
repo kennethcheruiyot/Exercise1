2e2018f431e7e53d7179a2183e948f782334a51a
kennethcheruiyot <kenneth.cheruiyot@strathmore.edu>
2023-10-03 17:34:40 +0300

    Add files via upload

---
+++ getUserInput.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>){
+    print("Enter a number: ")
+    val stringInput = readLine()!!
+    var interger:Int = stringInput.toInt()
+    println("You entered: $interger")
+}
